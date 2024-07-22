93437e303c006434a96010337ce8dd6c65c1791f
Pablo Aguirre <pablofabricio.aguirreguaman@studenti.unimi.it>
2024-07-22 17:06:14 +0200

    Starter Project

---
+++ WeatherApp/Models/Weather.swift
@@ -0,0 +1,16 @@
+//
+//  Weather.swift
+//  WeatherApp
+//
+//  Created by Pablo Aguirre on 22/07/24.
+//
+
+import Foundation
+
+struct WeatherResponse: Decodable {
+    
+}
+
+struct Weather: Decodable {
+    
+}
